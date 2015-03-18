ee1e1743d39213cbdc9a65a0e75ab98b795f1810
pandurang90 <pandurang.plw@gmail.com>
2015-03-18 22:35:09 +0530

    improvement in code

--- viki.rb
+++ viki.rb
@@ -4,8 +4,16 @@ require './celebrity'
 
 class Viki
   #This method returns true if there are duplicates in contents otherwise false
-  def self.check_for_duplicate_content(content_ids)
-    has_duplicate = false
+  def check_for_duplicate_content(contents)
+    if has_duplicate? contents.map(&:id)
+      puts "There are duplicates in #{contents.first.class.to_s} content"
+    else
+    	puts "There are no duplicates in #{contents.first.class.to_s} content"
+    end
+  end
+
+  def has_duplicate?(content_ids)
+  	has_duplicate = false
     content_ids.each do |content_id|
       if content_ids.count(content_id) > 1
         has_duplicate = true
@@ -21,20 +29,26 @@ end
 videos = []
 channels = []
 celebrity = []
-
+v = Viki.new
 [1,2,3,4,5].each do |number|
 	videos.push Video.new(number,"#{number}_title")
 	channels.push Channel.new(number,"#{number}_name")
 	celebrity.push Celebrity.new(number,"#{number}_name")
 end
 
-puts "This is example with no duplicate content"
-puts Viki.check_for_duplicate_content videos.map(&:id)
-puts Viki.check_for_duplicate_content channels.map(&:id)
-puts Viki.check_for_duplicate_content celebrity.map(&:id)
+puts ""
+puts "This is an example with no duplicate content"
+puts ""
+v.check_for_duplicate_content videos
+v.check_for_duplicate_content channels
+v.check_for_duplicate_content celebrity
 
 ############################ Without duplicates end ##############################
 
+
+puts ""
+puts "***********************"
+puts ""
 
 
 ############################ With duplicates ##############################
@@ -48,10 +62,11 @@ celebrity = []
 	celebrity.push Celebrity.new(number,"#{number}_name")
 end
 
-puts "This is example with no duplicate content"
-puts Viki.check_for_duplicate_content videos.map(&:id)
-puts Viki.check_for_duplicate_content channels.map(&:id)
-puts Viki.check_for_duplicate_content celebrity.map(&:id)
+puts "This is an example with no duplicate content"
+puts ""
+v.check_for_duplicate_content videos
+v.check_for_duplicate_content channels
+v.check_for_duplicate_content celebrity
 
 ############################ With duplicates end ##############################
 
